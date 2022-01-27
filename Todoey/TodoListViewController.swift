f0c82d358990042b84f021beb5bffcd3efffe209
vy-shoe <vadimulam6@gatech.edu>
2022-01-26 23:07:18 -0500

    Checkmark view and add item to todo array

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let items = ["Find Mike", "Buy eggos", "Destroy Demagorgan"]
+    var items = ["Find Mike", "Buy eggos", "Destroy Demagorgan"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,38 @@ class TodoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = items[indexPath.row]
         return cell
+    }
+    
+    //MARK: TableView Delgate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        // toggle checkmark of the list item
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+    }
+    
+    //MARK: Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        //what will happen once user clicks the Add Item button on UIAlert
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.items.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Buy milk, bread, eggs..."
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 
